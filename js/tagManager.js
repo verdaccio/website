2f5f67fc58805ec712d95ec791116e8058a2e108
Verdaccio bot for Deployments <verdacciobot@users.noreply.github.com>
2020-05-10 06:45:50 +0000

    Deploy website
    Deploy website version based on 761217882f83d307d2d76506813464fd2f119e82

--- js/tagManager.js
+++ js/tagManager.js
@@ -1,4 +1,4 @@
-window.dataLayer = [];
+window.dataLayer = window.dataLayer || [];
 
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
