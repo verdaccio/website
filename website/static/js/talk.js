e16a82948a0031a4a6e8166b7c0edbdbbf0b85aa
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2020-06-13 18:09:19 +0200

    Update talk.js

--- website/static/js/talk.js
+++ website/static/js/talk.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
     <div>Next talk 🥳 at <b>OpenJS World 2020</b></div> 
     <div>
       <p>
-        <a href="https://sched.co/bwIo" id="talk" target="_blank">Cover your Projects with a Multi-purpose Lightweight Node.js Registry - Juan Carlos Picado Herrera, eBay</a>
+        <a href="https://sched.co/bwIo" id="talk" target="_blank">Cover your Projects with a Multi-purpose Lightweight Node.js Registry - Juan Picado, eBay</a>
         <div class="note">Tuesday, June 23 • 2:25pm - 2:55pm Central Daylight Time (CDT), UTC -5.</div>
       </p>
     </div>
