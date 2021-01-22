4b3d107aba1ad4b2eb777f8078bf24f9ae950225
Verdaccio bot for Deployments <verdacciobot@users.noreply.github.com>
2021-01-22 20:36:14 +0000

    Deploy website
    Deploy website version based on 775badddb8d08ed55bdc36c744e96e2a890ae14e

--- js/talk.js
+++ js/talk.js
@@ -1,11 +1,10 @@
 $( document ).ready(function() {
   $('.post .postHeader').before(`<div class="banner">
 
-    <div>Next talk 🥳 at <b>#TheDestroDevShow</b></div> 
+    <div>Last talk 🥳 at <b>#TheDestroDevShow</b>. <b>Watch it now !!</b></div> 
     <div>
       <p>
         <a href="https://www.youtube.com/watch?reload=9&v=P_hxy7W-IL4" id="talk" target="_blank">Verdaccio - A lightweight Private Proxy Registry built in Node.js | Juan Picado</a>
-        <div class="note">Firday, 22th January 2021 • 6:00pm CET</div>
       </p>
     </div>
     </div>`);
