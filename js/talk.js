53dfd8f8625b77ffeba315145e6383ec10e5a08a
Verdaccio bot for Deployments <verdacciobot@users.noreply.github.com>
2021-06-14 18:19:45 +0000

    Deploy website
    Deploy website version based on 1fdb69835e2dff78e841bcde41c52c5e9eb1f83a

--- js/talk.js
+++ js/talk.js
@@ -1,10 +1,10 @@
 $( document ).ready(function() {
   $('.post .postHeader').before(`<div class="banner">
 
-    <div>Next talk 🥳 at <b>#DevSecCon Germany</b>. <b>RSVP now !!</b></div> 
+    <div>Next talk 🥳 at <b>#BeerJS Cordoba 🇦🇷 #53 in Spanish  </b>. <b>RSVP now !!</b></div> 
     <div>
       <p>
-        <a href="https://www.meetup.com/es-ES/devseccon-germany/events/276990087/" id="talk" target="_blank">Protecting my Node.js project of dependency confusion attacks | Juan Picado</a>
+        <a href="https://www.meetup.com/es-ES/Beer-js-Cordoba/events/278831479/" id="talk" target="_blank">BeerJS #53 Cordoba, Argentina | Juan Picado</a>
       </p>
     </div>
     </div>`);
